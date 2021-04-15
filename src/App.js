707b49311fe34e661d7c878ced58f1c6c28387c6
Jas <c.jasminher@gmail.com>
2021-04-14 22:47:09 -0400

    Added basic structure to page, such as select options for rates, miles driven, and hours. Added a test graph and added some css to structure out the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Bar } from "react-chartjs-2";
 
 function App() {
+  const [miles, setMiles] = useState(1000); //setting state as 1000
+  const data = {
+    labels: ["Rate A", "Rate B"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "blue",
+        borderColor: "blue",
+        borderWidth: 1,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    ],
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="select-options">
+        <label for="rates" id="rates-label">
+          Choose your rate
+        </label>
+        <select name="rates" id="rates">
+          <option value="rate a">Rate A</option>
+          <option value="rate b">Rate B</option>
+        </select>
+
+        <label for="miles" id="miles-label">
+          Miles driven per year
+        </label>
+        <input
+          type="range"
+          id="miles"
+          name="miles"
+          min="1000"
+          max="10000"
+          step="1000"
+          onChange={(e) => setMiles(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          {/* //here I setting the state so whenever I move the slider it changes the text */}
+        </input>
+
+        {/* and here it will display that text */}
+        <label for="miles" id="miles-range">
+          {miles}
+        </label>
+
+        <label for="hours" id="hours-label">
+          Choose your rate
+        </label>
+        <select name="hours" id="hours">
+          <option value="option 1">Between 5am and 9am</option>
+          <option value="option 2">Between noon and 6pm</option>
+          <option value="option 3">All hours (Unpredicatable)</option>
+        </select>
+      </div>
+
+      <div className="bar-graph">
+        <h1 className="bar-graph-title">Rate Comparison</h1>
+        <Bar
+          data={data}
+          width={250}
+          height={150}
+          options={{
+            maintainAspectRatio: true,
+          }}
+        />
+      </div>
     </div>
   );
 }
