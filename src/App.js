961a9d7a005fd54360c307288ab252ec693ffd45
Jas <c.jasminher@gmail.com>
2021-04-16 00:49:14 -0400

    Added equations for Rate A, Rate B off peak and Rate B peak. Added state to manage the value for the options hours the user will choose to input into equation for rates B.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,41 @@ import { Bar } from "react-chartjs-2";
 
 function App() {
   const [miles, setMiles] = useState(1000); //setting state as 1000
+  const [hours, setHours] = useState('Non Peak Hours');
+  
+  let rateA = 2973.15358 + (.3 * miles * .15); //rate A with rate $0.15/kWh 
+  let rateBPeek = 2266.93177 + (.3 * miles * .2); //rate B with rate $0.20/kwH peek hours 12pm - 6pm
+  let rateBOffPeek = 2266.93177 + (.3 * miles * .08); //rate B with rate $0.08/kwH off peek hours, hours otherwise
+  
+
+  console.log(hours);
+
   const data = {
     labels: ["Rate A", "Rate B"],
     datasets: [
       {
-        label: "My First dataset",
-        backgroundColor: "blue",
-        borderColor: "blue",
-        borderWidth: 1,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255,99,132,1)",
-        data: [65, 59, 80, 81, 56, 55, 40],
+        label:"Electric Bill",
+        data: [rateA, hours === "Between noon and 6pm" ? rateBPeek : rateBOffPeek],
+        backgroundColor: "rgb(58, 17, 184)",
       },
+    
+     
     ],
   };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  }
+
+
 
   return (
     <div className="App">
@@ -53,23 +74,17 @@ function App() {
         <label for="hours" id="hours-label">
           Choose your hours
         </label>
-        <select name="hours" id="hours">
-          <option value="option 1">Between 5am and 9am</option>
-          <option value="option 2">Between noon and 6pm</option>
-          <option value="option 3">All hours (Unpredictable)</option>
+        <select name="hours" id="hours" onChange={(e) => setHours(e.target.value)}>
+          <option value="Non Peak Hours">Non Peak Hours</option>
+          <option value="Between noon and 6pm">Between noon and 6pm</option>
+          {/* <option value="(Unpredictable)">All hours (Unpredictable)</option> */}
         </select>
       </div>
 
       <div className="bar-graph">
         <h1 className="bar-graph-title">Rate Comparison</h1>
-        <Bar
-          data={data}
-          width={320}
-          height={150}
-          options={{
-            maintainAspectRatio: true,
-          }}
-        />
+
+        <Bar data={data} options={options} width={320} height={150} />
       </div>
     </div>
   );
