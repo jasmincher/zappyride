59d9596980324a63e9e485d0c6f811e378ee6d10
Jas <c.jasminher@gmail.com>
2021-04-17 16:18:17 -0400

    Added a few comments

--- src/App.js
+++ src/App.js
@@ -67,6 +67,9 @@ function App() {
   let savings = rateATotal < rateBTotal ? rateBTotal - rateATotal : rateATotal - rateBTotal; //here I'm substracting the lowest rate from the highest rate to get the amount of savings
   let saveMore = rateATotal < rateBTotal ? "Rate A" : "Rate B"; //setting a string to use latter based on which rate is greater
 
+
+  //if the rate that is the lowest matches rate selected by user return the if statement 
+  //otherwise return the else statement
   const switchRates = () => {
     if (saveMore === rate) {
       return <p>You are on <span className="bolder">{rate}</span>, you are already on the cheapest rate</p>
@@ -138,6 +141,7 @@ function App() {
         />
       </div>
 
+      {/* used getwaves.io to make this svg */}
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
@@ -149,7 +153,7 @@ function App() {
           d="M0,160L80,144C160,128,320,96,480,117.3C640,139,800,213,960,240C1120,267,1280,245,1360,234.7L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>
       </svg>
-      
+
     </div>
   );
 }
