6504fccb091eae1e4e41efc5a3fb75e5ff712df6
Jas <c.jasminher@gmail.com>
2021-04-16 20:49:38 -0400

    Added state to keep track of value of rate when it is changed, to then use this state in a function that will return a string if the rate matches the rate that is the lowest, then the string that will be return is going to state the user is on the chepeast rate, otherwise it will suggest the user to switch to the other rate

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import React, { useState } from "react";
 import { Bar } from "react-chartjs-2";
 
 function App() {
-  const [miles, setMiles] = useState(1000); //setting state as 1000
-  const [hours, setHours] = useState("Non Peak Hours");
-
-  // let rateA = 2973.15358 + (.3 * miles * .15); //rate A with rate $0.15/kWh
-  // let rateBPeek = 2266.93177 + (.3 * miles * .2); //rate B with rate $0.20/kwH peak hours 12pm - 6pm
-  // let rateBOffPeek = 2266.93177 + (.3 * miles * .08); //rate B with rate $0.08/kwH off peek hours, hours otherwise
+  const [miles, setMiles] = useState(1000); //setting state for miles as 1000
+  const [hours, setHours] = useState("Non Peak Hours"); //setting state for hours as non peak
+  const [rate, setRate] = useState("Rate A"); //setting state for rate, the default to be Rate A
 
   let rateA = 2973.15358; //rate A with rate $0.15/kWh
   let rateB = 2266.93177; //rate B with rate $0.20/kwH peak hours 12pm - 6pm or with rate $0.08/kwH off peek hours
 
+  //parameters for graph
   const data = {
     labels: ["Rate A", "Rate B"],
     datasets: [
       {
         label: "Electric Bill",
-        data: [
-          rateA,
-          rateB,
-        ],
+        data: [rateA, rateB],
         backgroundColor: "rgb(58, 17, 184)",
       },
       {
@@ -36,8 +31,8 @@ function App() {
       },
     ],
   };
-  
 
+  //parameters for graph
   const options = {
     scales: {
       yAxes: [
@@ -64,22 +59,35 @@ function App() {
     },
   };
 
+  let rateATotal = data.datasets[0].data[0] + data.datasets[1].data[0]; //based on the graph I'm finding the electric bill plus the ev bill for rate A
+  let rateBTotal = data.datasets[0].data[1] + data.datasets[1].data[1]; //based on the graph I'm finding the electric bill plus the ev bill for rate B
 
-  let rateATotal =   data.datasets[0].data[0] +  data.datasets[1].data[0]; //based on the graph I'm finding the electric bill plus the ev bill for rate A
-  let rateBTotal =   data.datasets[0].data[1] + data.datasets[1].data[1]; //based on the graph I'm finding the electric bill plus the ev bill for rate B
-  let savings = rateATotal < rateBTotal ? rateBTotal - rateATotal : rateATotal - rateBTotal; //here I'm substracting the lowest rate from the highest rate to get the amount of savings
-  let saveMore =  rateATotal < rateBTotal ? "Rate A" : "Rate B"; //setting a string to use latter based on which rate is greater
+  //turn into a function , write into a test?
+  let savings =
+    rateATotal < rateBTotal ? rateBTotal - rateATotal : rateATotal - rateBTotal; //here I'm substracting the lowest rate from the highest rate to get the amount of savings
+  let saveMore = rateATotal < rateBTotal ? "Rate A" : "Rate B"; //setting a string to use latter based on which rate is greater
 
+  const switchRates = () => {
+    if (saveMore === rate) {
+      return `You are on ${rate}, you are already on the cheapest rate`;
+    } else {
+      return `You are currently on ${rate}, you can save more if you choose ${saveMore}, you can save $${savings.toFixed(2)}`;
+    }
+  };
 
   return (
     <div className="App">
       <div className="select-options">
         <label for="rates" id="rates-label">
-          Choose your rate
+          Choose your current rate
         </label>
-        <select name="rates" id="rates">
-          <option value="rate a">Rate A</option>
-          <option value="rate b">Rate B</option>
+        <select
+          name="rates"
+          id="rates"
+          onChange={(e) => setRate(e.target.value)}
+        >
+          <option value="Rate A">Rate A</option>
+          <option value="Rate B">Rate B</option>
         </select>
 
         <label for="miles" id="miles-label">
@@ -112,17 +120,16 @@ function App() {
         >
           <option value="Non Peak Hours">Non Peak Hours</option>
           <option value="Between noon and 6pm">Between noon and 6pm</option>
-          {/* <option value="(Unpredictable)">All hours (Unpredictable)</option> */}
         </select>
       </div>
 
       <div className="bar-graph">
         <h1 className="bar-graph-title">Rate Comparison</h1>
-        
-        <div id="save-more">you can save more if you choose {saveMore}, you can save ${savings.toFixed(2)}</div>
-        <Bar data={data} options={options} width={320} height={150} />
-  
 
+        <div id="switch-rates">
+          {switchRates()}
+        </div>
+        <Bar data={data} options={options} width={320} height={150} />
       </div>
     </div>
   );
